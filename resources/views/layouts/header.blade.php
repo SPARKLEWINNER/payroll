802b35b3a8d7056984296b77457d9bb678a380a2
renzcabato1 <cabato.renz.renz@gmail.com>
2023-02-28 12:05:42 +0800

    Done

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -37,8 +37,8 @@
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
           <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
-            <a class="navbar-brand brand-logo me-5" href="{{url('/')}}"><img src="{{ asset('images/logona.png') }}" class="me-2" alt="logo"/></a>
-            <a class="navbar-brand brand-logo-mini" href="{{url('/')}}"><img src="{{ asset('images/logona.png') }}" alt="logo"/></a>
+            <a class="navbar-brand brand-logo me-5" href="{{url('/')}}"><img src="{{ asset('images/icon.png') }}" class="me-2" alt="logo"/></a>
+            <a class="navbar-brand brand-logo-mini" href="{{url('/')}}"><img src="{{ asset('images/icon.png') }}" alt="logo"/></a>
           </div>
           <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
             <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -49,7 +49,7 @@
              
               <li class="nav-item nav-profile dropdown">
                 <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
-                  <img src="../../images/faces/face28.jpg" alt="profile"/>
+                  <img src="{{ asset('images/no_image.png') }}" alt="profile"/>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
                   <a class="dropdown-item">
