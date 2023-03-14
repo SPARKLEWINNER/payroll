2f53064dae1b7ddaa1737eedecb22878bbccde90
renzcabato1 <cabato.renz.renz@gmail.com>
2023-03-14 12:31:17 +0800

    Schedule API

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('attendance','AttendanceController@create')->name('create-attendance');
+Route::post('schedule','ScheduleController@create')->name('create-schedule');
 
