6d8fb50e6f9dd5fbdf779a2a10191f89f9dc8677
renzcabato1 <cabato.renz.renz@gmail.com>
2023-03-15 14:18:09 +0800

    Attendance Change hrs format to 24 hours

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -13,14 +13,14 @@ class AttendanceController extends Controller
         $attendance->emp_id = $request->emp_id;
         $attendance->emp_name = $request->emp_name;
         $attendance->status = $request->status;
-        $attendance->time = date('Y-m-d h:i:s',strtotime($request->time));
+        $attendance->time = date('Y-m-d H:i:s',strtotime($request->time));
         $attendance->store = $request->store;
         $attendance->remarks = url('');
         $attendance->date = date('Y-m-d',strtotime($request->date));
         $attendance->record_id = $request->record_id;
         $attendance->save();
 
-        return ['success',
+        return ['status' => 'success',
                 'data' => $attendance];
 
     }
